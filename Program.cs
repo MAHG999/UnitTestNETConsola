72102d342000c545f515b7a520393639eeeaf488
Marco <antnyhern@gmail.com>
2021-04-15 11:21:22 -0500

    Suebiendo codigo completo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PruebaUnitaria
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static string something()
+        {
+            return "algo";
+        }
+        public static bool LogIn(string User, string Password) => User == "Marco" && Password == "123456" ? true : false;
+    }
+}
